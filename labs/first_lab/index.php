6197dca1b8345139b8b9fea80b676589a9338aed
stl2802 <anmaks93@gmail.com>
2025-02-11 12:14:57 +0300

    first_com

---
+++ labs/first_lab/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Приветствие</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="" href="ini.php">
+</head>
+<body>
+    <header>
+        <div class="header__wrapper">
+            <img src="https://static.tildacdn.com/tild6162-3938-4539-b030-343364343938/1643631757_29-papik-.jpg"><h1>Максимов Андрей лабораторная работа 1 "Hello, World!"</h1>
+        </div>
+    </header>
+    <main>
+        <section>
+            <div class="section__wrapper">
+                <p class="diskr" id="w">
+                    <?php echo "Wassup world (101 интернет)!"; ?>
+                </p>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div class="footer__wrapper">
+            <div>
+                <p>
+                    Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий.
+                </p>
+            </div>
+            <div>
+                <a href="https://github.com/stl2802/php-dev.git" target="_blank">ссылка на гит</a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
